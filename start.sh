052bba91d69adc934961d714a4fac33a4f7795a8
Caio Coutinho <caiohcoutinho@gmail.com>
2020-01-24 22:16:56 -0300

    database credentials
    Change-Id: I0118c6055cf6e2bbed2d26731cc86783323af3d4

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 #sudo service apache2 stop;
 
-export PGUSER=vam5;
-export PGHOST=localhost;
-export PGPASSWORD=vam5;
-export PGDATABASE=vam5;
+export PGUSER=wltzyxhf;
+export PGHOST=rajje.db.elephantsql.com;
+export PGPASSWORD=zKnRa9cOjBYbb2gxTDGpuWewrge6nMjh;
+export PGDATABASE=wltzyxhf;
 export PGPORT=5432;
 node server.js;
